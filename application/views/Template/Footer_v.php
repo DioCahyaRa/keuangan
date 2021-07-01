a3bd8d144799cd7a46c15a3e0918065e34e95581
DioSkuy <dioskuy@DioSkuys-MacBook-Pro.local>
2021-07-01 20:50:38 +0700

    add javascript terbilang function

--- application/views/Template/Footer_v.php
+++ application/views/Template/Footer_v.php
@@ -18,6 +18,23 @@
     $('#example').DataTable();
     } );
     </script>
+
+    <!-- Terbilang -->
+    <script src="<?= base_url('Assets/terbilang/')?>jquery-1.11.2.min.js"></script>
+    <script src="<?= base_url('Assets/terbilang/')?>jquery.mask.min.js"></script>
+    <script src="<?= base_url('Assets/terbilang/')?>terbilang.js"></script>
+    <script type="text/javascript">
+    function inputTerbilang() {
+      //membuat inputan otomatis jadi mata uang
+      $('.mata-uang').mask('0.000.000.000', {reverse: true});
+
+      //mengambil data uang yang akan dirubah jadi terbilang
+       var input = document.getElementById("terbilang-input").value.replace(/\./g, "");
+
+       //menampilkan hasil dari terbilang
+       document.getElementById("terbilang-output").value = terbilang(input).replace(/  +/g, ' ');
+    } 
+  </script>
 </body>
 
 </html>
